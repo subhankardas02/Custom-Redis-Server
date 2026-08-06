b959f0b63477d5195c812ccacdbf98575dfa6f64
Subhankar Das <subhankardas2428@gmail.com>
2026-08-06 11:28:04 +0000

    Set socket options

--- main.cpp
+++ main.cpp
@@ -11,15 +11,17 @@ int main(){
     // int fd=socket();
     // connect(fd, addr);
 
-    struct MyString{
-        char* data;
-        size_t length;
-        size_t capacity;
+    // struct MyString{
+    //     char* data;
+    //     size_t length;
+    //     size_t capacity;
 
-    };
+    // };
     
     int fd=socket(AF_INET, SOCK_STREAM, 0);
-    
+    int val=1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
 
     
 

ebff0d26c662966b3fb0763e5112195cd7c72216
Subhankar Das <subhankardas2428@gmail.com>
2026-08-06 13:15:45 +0000

    connection stablished

--- main.cpp
+++ main.cpp
@@ -36,16 +36,21 @@ int main(){
     if(rv) {
         die("bind()");
     }
-
-    struct sockaddr_in {
-        uint16_t sin_family; // address family (AF_INET)
-        uint16_t sin_port;   // port in big endian
-        struct in_addr sin_addr; // IPv4 address
-    };
-    struct in_addr{
-        uint32_t s_addr; // IPv4 address in big endian
-    };
     
+    // struct sockaddr_in {
+    //     uint16_t sin_family; // address family (AF_INET)
+    //     uint16_t sin_port;   // port in big endian
+    //     struct in_addr sin_addr; // IPv4 address
+    // };
+    // struct in_addr{
+    //     uint32_t s_addr; // IPv4 address in big endian
+    // };
+    rv=listen(fd, SOMAXCONN);
+    if(rv){
+        die("listen()");
+    }
+
+
 
     
 

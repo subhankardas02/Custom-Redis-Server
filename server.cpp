6f1e43329db75ce5ed01b863de9c7418887a9983
Subhankar Das <subhankardas2428@gmail.com>
2026-08-08 19:31:30 +0000

    continue

--- server.cpp
+++ server.cpp
@@ -14,8 +14,9 @@ using namespace std;
 
 
 const size_t k_max_msg=4096;
+const size_t MAX_CONNECTIONS=1024;
 
-void die(const char* msg) {
+static void die(const char* msg) {
     perror(msg);
     exit(EXIT_FAILURE);
 }
@@ -116,22 +117,122 @@ static int32_t one_request(int connfd){
 int main(){
     
     int fd=socket(AF_INET, SOCK_STREAM, 0);
+    if(fd<0){
+        die("socket()");
+    }
+
     int val=1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+     
+
+    // Make listening socket non-blocking
+    fd_set_nonblocking(fd);
+
+
 
     // Bind to an address
-    struct sockaddr_in addr={}; // it's holding an IPv4
+    struct sockaddr_in addr={}; // it's holding an IPv4 address
     addr.sin_family=AF_INET;
     addr.sin_port=htons(1234); // Port
     addr.sin_addr.s_addr=htonl(0); // wildcard IP address
     int rv=bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
-    if(rv) {
+
+    if(rv){
         die("bind()");
     }
     rv=listen(fd, SOMAXCONN);
     if(rv){
         die("listen()");
     }
+    printf("Server listening on port 1234...\n");
+
+    Conn* connections[MAX_CONNECTIONS] = {nullptr};  // Array to hold active connections
+
+    struct pollfd pollfds[MAX_CONNECTIONS + 1]; // +1 for the listening socket
+
+    while(true){
+
+        int nfds = 1; // Start with the listening socket
+        
+        pollfds[0].fd = fd;
+        pollfds[0].events = POLLIN; // We want to know when we can accept a new connection
+        pollfds[0].revents = 0;
+
+        for(int i=0; i<Max_CONNECTIONS; i++){
+            if(connections[i]==nullptr) continue;
+
+            pollfds[nfds].fd = connections[i]->fd;
+            pollfds[nfds].events = POLLIN; // We want to know when we can read from the connection
+
+            if(connections[i]->wbuf_size > connections[i]->wbuf_sent){
+                pollfds[nfds].events |= POLLOUT; // We also want to know when we can write to the connection
+            }
+            pollfds[nfds].revents = 0;
+            nfds++;
+
+        }
+
+        int poll_rv=poll(pollfds, nfds, -1);
+        if(poll_rv<0){
+            if(errno==EINTR) continue; // Retry if interrupted by signal
+            die("poll()");
+        }
+
+        if(pollfds[0].revents & POLLIN){
+            while(true){
+                struct sockaddr_in client_addr{};
+                socklen_t addrlen=sizeof(client_addr);
+                int connfd=accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+                if(connfd<0){
+                    if(errno==EINTR) continue; // Retry if interrupted by signal
+                    if(errno==EAGAIN || errno==EWOULDBLOCK) break; // No more connections to accept
+                    perror("accept()");
+                    break;
+                }
+
+                // make client socket non-blocking
+                fd_set_nonblocking(connfd);
+                printf("Accepted new connection: fd=%d\n", connfd);
+
+                // Find a free slot for the new connection
+                bool added=false;
+                for(int i=0; i<MAX_CONNECTIONS; i++){
+                    if(connections[i]==nullptr){
+                        Conn* conn=new Conn();
+                        conn->fd=connfd;
+                        connections[i]=conn;
+                        added=true;
+                        break;
+                    }
+                }
+                if(!added){
+                    msg("Too many connections, closing new connection");
+                    close(connfd);
+                }
+
+            }
+        }
+
+        // Handle existing connections
+        for(int i=0; i<MAX_CONNECTIONS; i++){
+
+            Conn* conn=connections[i];
+            if(conn==nullptr) continue;
+            short revents=pollfds[i+1].revents; // +1 because pollfds[0] is the listening socket
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
 
     //  Accept connections
     while(true){
